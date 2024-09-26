3f04b38f45e6b83e7861d8ec76f3c5921d06a6cf
Dosbol Aliev <50006912+dosbolacademic@users.noreply.github.com>
2024-09-25 22:34:40 -0400

    Update p1.py

--- Unit2/Session1/p1.py
+++ Unit2/Session1/p1.py
@@ -1,47 +1,29 @@
 # Definition for singly-linked list.
-class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
-         
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def swapPairs(self, head):
-        if not head or not head.next:
-            return head
-        
-        dummy = ListNode(0)    
-        dummy.next=head
-        
-        first = head
-        second = head.next
-        temp = dummy
-        
-        while first and second:
-            temp.next = second
-            temp = temp.next
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = ListNode(0)
+        temp.next = head
+        prev = temp
+
+        while prev.next and prev.next.next:
+            
+            first = prev.next
+            second = prev.next.next
+
+            # First is now pointing to 3rd Node and 2nd Node is no Longer Availbe
             first.next = second.next
-            second.next = first #temp.next = first
-            temp = temp.next
-            first = first.next
-            if first:
-                second = first.next
 
-        if first:
-            temp.next = first
+            # second is pointing to first
+            second.next = first
 
-        return dummy.next
+            # prev is pointing to second after switch
+            prev.next = second
 
-        
+            # after swapping it is now our new prev to continue
+            prev = first
 
-
-s = "III"
-solution = Solution()
-print(solution.func(s))
-
-#1st pointer
-#2nd pointer
-
-#dummy node + tail pointer;
-
-#base cases:
-# empty || head.next empty -> return list
+        return temp.next
