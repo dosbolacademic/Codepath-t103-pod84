5db15ed826b4dc90fbb78e72d9e75b893d32d2af
Dosbol Aliev <50006912+dosbolacademic@users.noreply.github.com>
2024-09-30 00:05:52 -0400

    Update 1_shifting_letters.py

--- Unit2/2_strings_and_arrays/1_shifting_letters.py
+++ Unit2/2_strings_and_arrays/1_shifting_letters.py
@@ -1,1 +1,10 @@
-print("Hello World")
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        
+        s = list(s)
+        for i in range(len(shifts)-2,-1,-1):
+            shifts[i]+=shifts[i+1]
+        
+        for i in range(len(s)):
+            s[i] = chr(((ord(s[i])-ord('a')+shifts[i]) % 26 + ord('a')))
+        return "".join(s)
